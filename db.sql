309e351aef7e7cca71e4c06ede3cacdec687eb55
Kinley Tshering <kinleytshering@Kinleys-MacBook-Pro.local>
2021-08-10 20:23:58 +0600

    db

---
+++ db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `boy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) DEFAULT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4
